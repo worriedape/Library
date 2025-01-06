7656695a3711ada7efd2bd6da1cd5a68f78cce20
worriedape <144045807+worriedape@users.noreply.github.com>
2025-01-06 17:06:36 -0600

    js functions refinements

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+const booksDisplay = document.querySelector(".books");
+
+function display() {
+  const oneBook = document.createElement("p");
+  myLibrary.map((i) => (oneBook.textContent = i));
+  booksDisplay.appendChild(oneBook);
 }
 
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
+
+function Book() {
+  this.title = title.value;
+  this.author = author.value;
+  this.pages = pages.value;
+  this.alreadyRead = function () {
+    read.checked ? "Yes" : "Not yet";
+  };
+}
+
+const addBook = document.getElementById("add-book");
+
 function addBookToLibrary() {
-  // do stuff here
+  addBook.addEventListener("click", () => {
+    myLibrary.push(new Book());
+    display();
+  });
 }
